c61495a617b273af8e22a32632ad89beb37bb1e4
kethlyn kruk <kethlynsk17@outlook.com>
2024-06-25 21:12:32 -0300

    imparEpar

---
+++ imparEpar.js
@@ -0,0 +1,12 @@
+// IF ELSE IMPAR PAR
+// console.log("Digite um número");
+// process.stdin.once('data', function (data){
+//         let resposta = Number(data.toString().trim());
+//         let resp = resposta % 2 === 0;
+
+//             if (resp){
+//                 console.log('Número Par' );
+//             } else {
+//                 console.log('Número Ímpar');}
+//             process.exit();
+//         })
